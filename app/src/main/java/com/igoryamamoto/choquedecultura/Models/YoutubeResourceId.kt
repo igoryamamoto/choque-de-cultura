1411634e6d3bb0c5fb9336ef80d541df6cab0c1d
Ígor Yamamoto <igor.a.r.y@gmail.com>
2018-05-09 19:08:57 -0300

    Create youtube models

---
+++ app/src/main/java/com/igoryamamoto/choquedecultura/Models/YoutubeResourceId.kt
@@ -0,0 +1,17 @@
+package com.igoryamamoto.choquedecultura.Models
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class YoutubeResourceId {
+
+    @Expose
+    @SerializedName("videoId")
+    var videoId: String? = null
+
+    /*
+    kind: "youtube#video",
+    videoId: "8VmbLF3-Aww"
+     */
+
+}
